59ffddba606be2065fa8a83bac0108eb9b65fbb2
chrifu4ever <G6xep2248b>
2018-03-10 15:35:33 +0100

    Database Connection

---
+++ Model/connectDB.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class ConnectDB
+{
+    public function connect()
+    {
+
+        $sql = new mysqli("127.0.0.1", "messenger", "ZYCDPzlg05cGkHM9", "Messenger");
+        mysqli_set_charset($sql,"utf8");
+        if ($sql->connect_errno) {
+            echo "Failed to connect to MySQL: (" . $sql->connect_errno . ") " . $sql->connect_error;
+        }
+
+       return $sql;
+    }
+}
+
+?>
