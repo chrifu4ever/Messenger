c79ce30ad4c6c15f4b2849310be494a61e71946c
chrifu4ever <G6xep2248b>
2018-03-10 15:36:12 +0100

    Database Connection

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Hallo</h1>
+<?php
+require("Model/connectDB.php");
+$db = new ConnectDB();
+$db->connect();
+?>
+</body>
+</html>
